2bb54559c2ac25e82f0717e174638dff34dbab8b
Dhanujvardhan <dhanujvardhan0123@gmail.com>
2026-05-17 16:45:38 +0530

    Optimized backend for Render

--- backend/app.py
+++ backend/app.py
@@ -1,54 +1,25 @@
-from flask import Flask, jsonify, Response
+from flask import Flask, jsonify
 from flask_cors import CORS
-from detection import detect_vehicles
-import cv2
+import random
 
 app = Flask(__name__)
 CORS(app)
 
-# API Route
+@app.route("/")
+def home():
+
+    return jsonify({
+        "message": "AI Traffic Backend Running"
+    })
+
 @app.route("/traffic")
 def traffic():
 
-    vehicle_count, frame = detect_vehicles()
+    vehicle_count = random.randint(1, 20)
 
     return jsonify({
         "vehicles": vehicle_count
     })
 
-# Video Feed Route
-@app.route("/video")
-def video():
-
-    def generate():
-
-        while True:
-
-            vehicle_count, frame = detect_vehicles()
-
-            ret, buffer = cv2.imencode('.jpg', frame)
-
-            frame_bytes = buffer.tobytes()
-
-            yield (
-                b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' +
-                frame_bytes +
-                b'\r\n'
-            )
-
-    return Response(
-        generate(),
-        mimetype='multipart/x-mixed-replace; boundary=frame'
-    )
-
-# Home Route
-@app.route("/")
-def home():
-
-    return jsonify({
-        "message": "Smart Traffic AI Backend Running"
-    })
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
